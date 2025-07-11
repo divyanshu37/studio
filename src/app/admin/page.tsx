fa10e0a80530aa55082f766a23bc58f4786f149b
Nate Embrey <nate@ranadev.io>
2025-07-11 21:21:39 +0000

    put completed at the end

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -56,18 +56,6 @@ export default function AdminPage() {
                     </p>
                 </CardContent>
             </Card>
-            <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Completed This Month</CardTitle>
-                    <CheckCircle className="h-4 w-4 text-muted-foreground" />
-                </CardHeader>
-                <CardContent>
-                    <div className="text-2xl font-bold">{stats.completions}</div>
-                    <p className="text-xs text-muted-foreground">
-                        Total applications completed this month
-                    </p>
-                </CardContent>
-            </Card>
              <Card>
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">Total Visits Last Month</CardTitle>
@@ -77,6 +65,18 @@ export default function AdminPage() {
                     <div className="text-2xl font-bold">{stats.visitsLastMonth}</div>
                      <p className="text-xs text-muted-foreground">
                         Total unique visitors last month
+                    </p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Completed This Month</CardTitle>
+                    <CheckCircle className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">{stats.completions}</div>
+                    <p className="text-xs text-muted-foreground">
+                        Total applications completed this month
                     </p>
                 </CardContent>
             </Card>
