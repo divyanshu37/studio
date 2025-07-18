c8fe842d20a9fab43b38ad7ddbea54a424a18d2b
ram chander Rao <ram.234rao@gmail.com>
2025-07-18 21:39:46 +0000

    so lets try again, i have enabled all the services that you would need f

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,22 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.BACKEND_URL,
   },
+  webpack: (config, { isServer }) => {
+    // This is a workaround for a warning in the 'handlebars' library.
+    // The library uses a deprecated feature ('require.extensions') that causes a warning in Webpack.
+    // This configuration tells Webpack to ignore this specific warning for this specific library.
+    config.module.rules.push({
+      test: /node_modules\/handlebars\/lib\/index\.js$/,
+      loader: 'string-replace-loader',
+      options: {
+        search: 'require.extensions',
+        replace: 'null',
+      },
+    });
+
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
